6b91b09a2de889687ac630d66655603186b9a2c6
suhib alabed <suhib.alabed@gmail.com>
2023-08-05 18:27:00 +0300

    'yt

--- src/android/com/outsystems/plugins/arexample/ARPlugin.kt
+++ src/android/com/outsystems/plugins/arexample/ARPlugin.kt
@@ -69,8 +69,8 @@ class ARPlugin: CordovaImplementation(){
             }
             else{
                 val intent = Intent(cordova.activity, ArTradeActivity::class.java)
-                intent.putExtra("obj_path", "./raw/mesh.obj")
-                intent.putExtra("texture_path", "./raw/diffuse.png")
+                intent.putExtra("obj_path", "res/raw/mesh.obj")
+                intent.putExtra("texture_path", "res/raw/diffuse.png")
 q
                 cordova.activity.startActivityForResult(intent, 1)
             }
